d2976e06bf60753193990a637a8591fded7fe6a5
andy <thamandyy@gmail.com>
2019-06-18 23:24:55 -0400

    Input and grab

--- gui.py
+++ gui.py
@@ -5,5 +5,21 @@ import tkinter as tk
 win = tk.Tk()
 win.title("ig grab")
 
+# input to grab URL
+inputLabel = tk.Label(win, text="test")
+inputLabel.pack()
+
+inputField = tk.Entry(win)
+inputField.pack()
+
+# func that does grabbing
+def grabInput(event):
+    url = inputField.get()
+    inputField.delete(0, tk.END )
+    print(url)
+
+inputField.bind('<Return>', grabInput)
+
+
 # event loop
 win.mainloop()
