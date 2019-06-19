2c6cc0536a0da11001f622f5b64e3406790f4cb1
andy <thamandyy@gmail.com>
2019-06-18 20:18:07 -0400

    Simplified

--- instagram.py
+++ instagram.py
@@ -6,7 +6,5 @@ url = "https://www.instagram.com/p/BwhVD2OBDvL/"
 r = requests.get(url)
 unparsedText = r.text
 soup = BeautifulSoup(unparsedText, "html.parser")
-metas = soup.find_all('meta')
-# img = metas.find(property="og:image")
-img = soup.find('meta',{'property' : 'og:image'})
-print(img.attrs['content'])
+imgUrl = soup.find('meta',{'property' : 'og:image'}).attrs['content']
+print(imgUrl)
