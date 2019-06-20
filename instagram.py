211c6dbbe72b13cfcfad57fa05d1e65cfa70986a
andy <thamandyy@gmail.com>
2019-06-20 00:16:35 -0400

    Refactored into function

--- instagram.py
+++ instagram.py
@@ -2,9 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 
 # test url of johnny daigo
-url = "https://www.instagram.com/p/BwhVD2OBDvL/"
-r = requests.get(url)
-unparsedText = r.text
-soup = BeautifulSoup(unparsedText, "html.parser")
-imgUrl = soup.find('meta',{'property' : 'og:image'}).attrs['content']
-print(imgUrl)
+# url = "https://www.instagram.com/p/BwhVD2OBDvL/"
+
+def grabUrl(url):
+    r = requests.get(url)
+    unparsedText = r.text
+    soup = BeautifulSoup(unparsedText, "html.parser")
+    imgUrl = soup.find('meta',{'property' : 'og:image'}).attrs['content']
+    return imgUrl
