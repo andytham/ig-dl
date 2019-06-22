839e7708a96c3865c59a460327b22e1d39178fc0
andy <thamandyy@gmail.com>
2019-06-22 19:25:51 -0400

    Cleaner code, properly check for videos (can be in albums)

--- instagram.py
+++ instagram.py
@@ -15,11 +15,6 @@ def scrapeUrl(url):
     postTitle = soup.find('link', {'rel' : 'canonical'}).attrs['href']
     splitTitle = postTitle.split("/")
     fileName = splitTitle[3] + " - " + splitTitle[5] # author + ig's hash
-
-
-    imgUrl = soup.find('meta',{'property' : 'og:image'}).attrs['content'] # old way to grab img url
-    # get the extension
-    ext = imgUrl.split('?')[0].split('.')[-1]
         
     # data is structured like this if in an album: window._sharedData.entry_data.PostPage[0].graphql.shortcode_media.edge_sidecar_to_children.edges
     # loops through edges to each node if album
@@ -28,25 +23,23 @@ def scrapeUrl(url):
     req = [fileName] # what we will return, starting with the filename
     # if album, else dump in single url
     shorthandJson = jsonDump['entry_data']['PostPage'][0]['graphql']['shortcode_media']
-    def parseUrl(url):
-        ext = url.split('?')[0].split('.')[-1]
-        mediaReqData = requests.get(url)
-        req.append([mediaReqData, ext])
+    def parseJsonIntoUrl(url):
+        if url['is_video']:
+            ext = url.split('?')[0].split('.')[-1]
+            mediaReqData = requests.get(url['video_url'])
+            req.append([mediaReqData, ext])
+        else:
+            ext = url.split('?')[0].split('.')[-1]
+            mediaReqData = requests.get(url['display_url'])
+            req.append([mediaReqData, ext])
 
     if 'edge_sidecar_to_children' in shorthandJson: # if it is an album (multi images)
         edges = shorthandJson['edge_sidecar_to_children']['edges']
         for node in edges:
-            url = node['node']['display_url']
-            parseUrl(url)
+            url = node['node']
+            parseJsonIntoUrl(url)
     else:
-        if shorthandJson['is_video'] is False:
-            url = shorthandJson['display_url']
-            parseUrl(url)
-        else:
-            url = shorthandJson['video_url']
-            parseUrl(url)
-    #TODO check if mp4
-
+        parseJsonIntoUrl(shorthandJson)
     return req
   
 # import env
