60e46830ff4d95166d3aac8d627903649e8a3d22
andy <thamandyy@gmail.com>
2019-06-22 19:32:35 -0400

    Changed what data types are returned in scrapUrl

--- instagram.py
+++ instagram.py
@@ -14,24 +14,25 @@ def scrapeUrl(url):
     # create the file name to save as
     postTitle = soup.find('link', {'rel' : 'canonical'}).attrs['href']
     splitTitle = postTitle.split("/")
-    fileName = splitTitle[3] + " - " + splitTitle[5] # author + ig's hash
+    filename = splitTitle[3] + " - " + splitTitle[5] # author + ig's hash
         
     # data is structured like this if in an album: window._sharedData.entry_data.PostPage[0].graphql.shortcode_media.edge_sidecar_to_children.edges
     # loops through edges to each node if album
     jsonData = soup.findAll(text=re.compile("window._sharedData"))[0][21:-1].encode(encoding='UTF-8',errors='strict')
     jsonDump = json.loads(jsonData)
-    req = [fileName] # what we will return, starting with the filename
+    req = {'filename': filename} # what we will return, starting with the filename
+    fileList = []
     # if album, else dump in single url
     shorthandJson = jsonDump['entry_data']['PostPage'][0]['graphql']['shortcode_media']
     def parseJsonIntoUrl(url):
         if url['is_video']:
             ext = url.split('?')[0].split('.')[-1]
             mediaReqData = requests.get(url['video_url'])
-            req.append([mediaReqData, ext])
+            fileList.append([mediaReqData, ext])
         else:
             ext = url.split('?')[0].split('.')[-1]
             mediaReqData = requests.get(url['display_url'])
-            req.append([mediaReqData, ext])
+            fileList.append([mediaReqData, ext])
 
     if 'edge_sidecar_to_children' in shorthandJson: # if it is an album (multi images)
         edges = shorthandJson['edge_sidecar_to_children']['edges']
@@ -40,6 +41,7 @@ def scrapeUrl(url):
             parseJsonIntoUrl(url)
     else:
         parseJsonIntoUrl(shorthandJson)
+    req.update({'fileList':fileList})
     return req
   
 # import env
